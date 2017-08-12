d17896856da84766c9e437de1f7c45b2150ccd89
Dayton Garcia <dayton.garcia@gmail.com>
2017-08-11 21:01:24 -0600

    code for test on quiz.py

--- controllers/quiz.py
+++ controllers/quiz.py
@@ -85,8 +85,9 @@ def obtenerQuiz():
 
 @auth.requires_login()
 def GuardarQuiz():
+    print 'Hola'
     import redis
-    r = redis.StrictRedis(host='localhost', port=6379, db=0);
+    r = redis.StrictRedis(host='127.0.0.1', port=6379, db=0);
     ide = request.vars['id']
     preguntas = request.vars['jsonquiz']
     curso = request.vars['project']
