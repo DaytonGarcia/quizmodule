722e74b4a8f16f14b6cb3279031541bd3f92d769
Dayton Garcia <dayton.garcia@gmail.com>
2017-08-12 00:07:43 -0600

    test

--- controllers/quiz.py
+++ controllers/quiz.py
@@ -45,7 +45,7 @@ def consult_quiz():
     for cuestionario in cuestionarios:
         cuestionario = cuestionario.split(':')
         creador = db.executesql("select concat(first_name,' ',+last_name) as name from auth_user where id = "+cuestionario[1])
-        curso = db.executesql("select name from project where project_id = "+cuestionario[3])
+        curso = db.executesql("select name from project where project_id = '"+cuestionario[3]+"'")
         print(creador)
         print(curso)
     return dict(ecys_var = ecys_var, periodo = period, project=project, idperiodoc=idperiodoc, idproject=idproject,a=a)
