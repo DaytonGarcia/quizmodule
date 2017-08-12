a24e01ec441dc9b6f1631c7e1815e006184dbac2
Dayton Garcia <dayton.garcia@gmail.com>
2017-08-11 21:04:24 -0600

    test

--- controllers/quiz.py
+++ controllers/quiz.py
@@ -85,7 +85,7 @@ def obtenerQuiz():
 
 @auth.requires_login()
 def GuardarQuiz():
-    print 'Hola'
+    print "Hola"
     import redis
     r = redis.StrictRedis(host='127.0.0.1', port=6379, db=0);
     ide = request.vars['id']
