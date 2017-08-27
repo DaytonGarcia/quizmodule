f78b84ffd845b6fa8128d799e867539b0e6ae711
Dayton Garcia <dayton.garcia@gmail.com>
2017-08-26 21:37:34 -0600

    test

--- static/js/quiz-js/quiz.js
+++ static/js/quiz-js/quiz.js
@@ -43,9 +43,7 @@ function GruardarQuiz()
         JsonQuiz += ']}';
         var parametros = "?id="+JsonCorretaltivo.value+"&jsonquiz="+JsonQuiz+"&uid="+userid+"&project="+projectid+"&title="+title;
         submitLoad("quiz","GuardarQuiz",parametros);
-        $('#popup').fadeIn('slow');
-        $('.popup-overlay').fadeIn('slow');
-        $('.popup-overlay').height($(window).height());
+        
         alert("Se ha guardado el cuestionario con id: "+JsonCorretaltivo.value);
         var par = "?period="+periodo_id+"&project="+idproject;
         
@@ -54,6 +52,9 @@ function GruardarQuiz()
     else
     {
         alert("No hay preguntas");
+        $('#popup').fadeIn('slow');
+        $('.popup-overlay').fadeIn('slow');
+        $('.popup-overlay').height($(window).height());
     }
     
 }
