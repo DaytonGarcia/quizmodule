b4ed03f41ed924e83fd54cc71c903fb93b32133f
Dayton Garcia <dayton.garcia@gmail.com>
2017-08-28 23:36:13 -0600

    test

--- controllers/quiz.py
+++ controllers/quiz.py
@@ -182,5 +182,5 @@ def getJsonQuiz():
         creador = quiz.creador
         curso = quiz.curso
     cadenaRedis = 'uid:'+str(creador)+':curso:'+str(curso)+':quiz:'+str(ide)
-    r.hget('preguntas',cadenaRedis)
+    r.hget('preguntas','uid:723:curso:0283A:quiz:20')
     return r
