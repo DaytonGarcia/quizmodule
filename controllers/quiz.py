7fc1a9e067490e95cea1cdbc633e0a9dd37419aa
Dayton Garcia <dayton.garcia@gmail.com>
2017-08-29 21:49:43 -0600

    test

--- controllers/quiz.py
+++ controllers/quiz.py
@@ -168,10 +168,10 @@ def viewer_quiz3():
 
 @auth.requires_login()
 def getJsonQuiz():
-    ide = tuple(request.args)
+    x = tuple(request.args)
     import redis
     r = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
-    #ide = request.vars['ide']
+    ide = x
     lista = db(db.tb_metadata_quiz.id_quiz==ide).select(
         db.tb_metadata_quiz.creador, 
         db.tb_metadata_quiz.curso)
