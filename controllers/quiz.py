75cf5e91f82734a93f28429815644769de997610
Dayton Garcia <dayton.garcia@gmail.com>
2017-09-05 22:25:18 -0600

    test

--- controllers/quiz.py
+++ controllers/quiz.py
@@ -263,10 +263,10 @@ def getActivities():
     print categoria
 
     actividades = db(
-        db.course_activity.assignation==int(curso),
-        db.course_activity.laboratory=='T',
-        db.course_activity.semester==int(period),
-        db.course_activity_category.category==int(categoria)
+        db.course_activity.assignation==curso &
+        db.course_activity.laboratory=='T' &
+        db.course_activity.semester==period &
+        db.course_activity_category.category==categoria
 
     ).select(
         db.course_activity.id,
