c80fbc5d50c8e60dd86a0df40f23180debdbf05a
Dayton Garcia <dayton.garcia@gmail.com>
2017-09-19 22:12:25 -0600

    test

--- controllers/quiz.py
+++ controllers/quiz.py
@@ -289,7 +289,7 @@ def getActivities():
     return response.json(actividades)
 
 def GetNow():
-    ahora = request.now # Obtiene fecha y hora actual
+    ahora = request.utcnow # Obtiene fecha y hora actual
     print("Fecha y Hora:", ahora)  # Muestra fecha y hora
     return ahora 
 
