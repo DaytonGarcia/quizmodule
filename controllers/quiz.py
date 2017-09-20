6423b73546edbe19116af2ff077a75b5451562bc
Dayton Garcia <dayton.garcia@gmail.com>
2017-09-19 22:58:09 -0600

    test

--- controllers/quiz.py
+++ controllers/quiz.py
@@ -395,7 +395,7 @@ def test_programacion_protegida():
             inicio= pInicio,
             duracion= pDuracion,
             finalizado= False,
-            private=  True,
+            private=  'True',
             keyword= pClave
             )
         db.commit()
