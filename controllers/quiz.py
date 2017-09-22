6e04df3dd1d546ce80b9f9822deb0af85af2db22
Dayton Garcia <dayton.garcia@gmail.com>
2017-09-22 17:16:10 -0600

    test

--- controllers/quiz.py
+++ controllers/quiz.py
@@ -124,7 +124,7 @@ def take_quiz():
 
     ##inicia codigo viejo
     ## termina codigo viejo
-    myquery = (db.vw_quiz_actividad.id_project==project) & (db.vw_quiz_actividad.semestre==period)
+    myquery = (db.vw_quiz_actividad.id_project==1) & (db.vw_quiz_actividad.semestre==8)
     
     programaciones = db(myquery).select(db.vw_quiz_actividad.ALL)
     print programaciones
