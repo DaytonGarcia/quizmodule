66496f71b4730fc3baff4b049d12ddd46134815b
Dayton Garcia <dayton.garcia@gmail.com>
2017-09-23 08:02:45 -0600

    test

--- static/js/quiz-js/quiz.js
+++ static/js/quiz-js/quiz.js
@@ -41,6 +41,7 @@ function GruardarQuiz()
         idproject = document.getElementById("id_project").name;
         title = document.getElementById("txtTitulo").value;
         JsonQuiz += ']}';
+        JsonQuiz = JsonQuiz.replace(';','.');
         var parametros = "?id="+JsonCorretaltivo.value+"&jsonquiz="+JsonQuiz+"&uid="+userid+"&project="+projectid+"&title="+title;
         submitLoad("quiz","GuardarQuiz",parametros);
         
