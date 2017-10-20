400bbf1d1b066994c4286b8b6af9604067b40172
Dayton Garcia <dayton.garcia@gmail.com>
2017-10-19 21:02:12 -0600

    test

--- controllers/quiz.py
+++ controllers/quiz.py
@@ -117,7 +117,7 @@ def GuardarQuizPost():
     import json
     print "Dos"
     r = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
-    JsonRecive = json.loads(request.post_vars)
+    JsonRecive = request.post_vars
     print JsonRecive
     ide = JsonRecive["Correlativo"]
     print ide
