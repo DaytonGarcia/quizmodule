8b640d36f8a7102d73cb478b5b1d5c86c0518c0e
Dayton Garcia <dayton.garcia@gmail.com>
2017-10-19 22:27:45 -0600

    test

--- static/js/quiz-js/quiz.js
+++ static/js/quiz-js/quiz.js
@@ -43,13 +43,13 @@ function GruardarQuiz()
         title = document.getElementById("txtTitulo").value;
         //JsonQuiz += ']}';
         JsonQuiz += ']';
-        //var parametros = "?id="+JsonCorretaltivo.value+"&jsonquiz="+JsonQuiz+"&uid="+userid+"&project="+projectid+"&title="+title;
+        var parametros = "?id="+JsonCorretaltivo.value+"&jsonquiz="+JsonQuiz+"&uid="+userid+"&project="+projectid+"&title="+title;
         //var parametros = '{"Correlativo":'+JsonCorretaltivo.value+', "jsonquiz":'+JsonQuiz+',"uid":'+userid+',"project":"'+projectid+'","title": "'+title+'"}';
         var parametros = "?id="+JsonCorretaltivo.value+"&uid="+userid+"&project="+projectid+"&title="+title;
         var datas = JsonQuiz;
         alert(parametros);
-        //submitLoad("quiz","GuardarQuiz",parametros);
-        SendPost("quiz","GuardarQuizPost",parametros,JsonQuiz);
+        submitLoad("quiz","GuardarQuiz",parametros);
+        //SendPost("quiz","GuardarQuizPost",parametros,JsonQuiz);
         
         alert("Se ha guardado el cuestionario con id: "+JsonCorretaltivo.value);
         var par = "?period="+periodo_id+"&project="+idproject;
