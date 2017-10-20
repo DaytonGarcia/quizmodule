bde52a17d9cae19df4531088691d2b57f251cdc3
Dayton Garcia <dayton.garcia@gmail.com>
2017-10-19 22:45:10 -0600

    test

--- controllers/quiz.py
+++ controllers/quiz.py
@@ -118,6 +118,8 @@ def GuardarQuizPost():
     
     preguntas = json.dumps(request.post_vars)
     preguntas = preguntas.replace('{\"','')
+    preguntas = preguntas.replace('": ""}','')
+    preguntas = preguntas.replace('\"','')
     ide = request.vars['id']
     curso = request.vars['project']
     uid = request.vars['uid']
