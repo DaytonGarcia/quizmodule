42a8fd709d827d8a967513626a6f37aca370e000
Dayton Garcia <dayton.garcia@gmail.com>
2017-10-22 19:37:03 -0600

    test

--- controllers/quiz.py
+++ controllers/quiz.py
@@ -239,7 +239,7 @@ def evaluacion():
     ##Si el quiz esta activo recupero el detalle
     if (activo == True):
         import json
-        JsonQuiz = unicode(JsonQuiz, 'latin-1')
+        JsonQuiz = unicode(JsonQuiz, 'utf-8')
         template_respuestas=json.loads(JsonQuiz)
 
         for pregunta in template_respuestas["PREGUNTAS"]:
